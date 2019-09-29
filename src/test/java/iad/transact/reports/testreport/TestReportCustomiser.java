3c36cbcb7721273ad32d83c76cf66534c65cd565
Josh Uzo <nwankwo.joshua@gmail.com>
2019-09-29 21:03:31 +1000

    Added repoort customisation

--- src/test/java/iad/transact/reports/testreport/TestReportCustomiser.java
+++ src/test/java/iad/transact/reports/testreport/TestReportCustomiser.java
@@ -4,8 +4,8 @@ import iad.reports.ColumnFieldPair;
 import iad.reports.Customiser;
 import iad.reports.CustomiserHelper;
 import net.sf.jasperreports.engine.JRBand;
-import net.sf.jasperreports.engine.JRCommonElement;
 import net.sf.jasperreports.engine.JRElement;
+import net.sf.jasperreports.engine.design.JRDesignElement;
 import net.sf.jasperreports.engine.design.JasperDesign;
 
 import java.util.*;
@@ -28,34 +28,57 @@ public class TestReportCustomiser implements Customiser<TestReportFakeData> {
         JRBand columnHeaderBand = design.getColumnHeader();
         List<String> columnKeys = new ArrayList<>();
         ALL_COLUMN_HEADER_KEYS.forEach(key -> columnKeys.add(key.getColumnKey()));
-
-
         List<JRElement> tableHeaderColumns = CustomiserHelper.getBandElementsByKeys2(columnHeaderBand, columnKeys);
-        System.out.println(tableHeaderColumns.size());
         int tableHeaderWidth = tableHeaderColumns.stream().mapToInt(JRElement::getWidth).sum();
-        System.out.println(tableHeaderWidth);
-
         List<JRElement> nonMifidTableHeaderColumns = new ArrayList<>(tableHeaderColumns);
-        
-        IntStream.range(0,nonMifidTableHeaderColumns.size()).forEach(index -> {
-
-            if (excludeKeys.contains(nonMifidTableHeaderColumns.get(index).getKey())) {
-
-                nonMifidTableHeaderColumns.remove(index);
-                System.out.println(index);
+        Iterator<JRElement> itr = nonMifidTableHeaderColumns.iterator();
+        while(itr.hasNext()) {
+            JRElement nextElement = itr.next();
+            if (excludeKeys.contains(nextElement.getKey())) {
+                itr.remove();
             }
-        });
-
-        System.out.println(nonMifidTableHeaderColumns.size());
-
+        }
+        int nonMifidTableHeaderWidth = nonMifidTableHeaderColumns.stream().mapToInt(JRElement::getWidth).sum();
+        double rebalanceFactor = (double) tableHeaderWidth / nonMifidTableHeaderWidth;
+        hideElements(excludeKeys, columnHeaderBand);
+        rebalanceColumn(nonMifidTableHeaderColumns, rebalanceFactor);
     }
 
-    public void run(){
+    private void rebalanceColumn(List<JRElement> columnElements, double rebalanceFactor) {
+        Integer[] totalWidth = { 0 };
+    IntStream.range(0, columnElements.size())
+        .forEach(
+            index -> {
+              JRElement column = columnElements.get(index);
+              double newWidth = (column.getWidth() * rebalanceFactor);
+              if (index == 0) {
+                column.setWidth((int) Math.round(newWidth));
+                  System.out.println(column.getKey());
+              } else {
+                column.setX(totalWidth[0]);
+                column.setWidth((int) Math.round(newWidth));
+              }
+              totalWidth[0] = totalWidth[0] + (int) Math.round(newWidth);
+            });
 
+/*        columnElements.forEach(
+            column -> {
+              double newWidth = (column.getWidth() * rebalanceFactor);
+                if(column.getKey().equals("nameColumnKey")){
+                    column.setWidth((int) Math.round(newWidth));
+                }else {
+                    column.setX(totalWidth[0]);
+                    column.setWidth((int) Math.round(newWidth));
+                }
+                totalWidth[0] = totalWidth[0] + (int) Math.round(newWidth);
+            });*/
+    }
+
+    private void hideElements(
+            List<String> keys, JRBand band) {
+        keys.forEach(
+                entry -> CustomiserHelper.hideElementByKey(band, entry));
     }
 
 
-    public static void main(String[] args) {
-        new TestReportCustomiser().customise(null,null);
-    }
 }
