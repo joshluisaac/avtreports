729db2ae19401990ffb797f4bba83839c9dfe684
Josh Uzo <nwankwo.joshua@gmail.com>
2019-10-02 08:31:48 +1000

    Updates

--- src/main/java/iad/reports/CustomiserHelper.java
+++ src/main/java/iad/reports/CustomiserHelper.java
@@ -1,10 +1,8 @@
 package iad.reports;
 
-import iad.transact.reports.corporateactionelection.ReportElement;
-
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
-
 import net.sf.jasperreports.engine.JRBand;
 import net.sf.jasperreports.engine.JRElement;
 import net.sf.jasperreports.engine.JRElementGroup;
@@ -26,9 +24,8 @@ public class CustomiserHelper {
   }
 
   // iterates over the keys to build report elements
-  public static Map<String, ReportElement> getBandElementsByKeys2(
-      JRElementGroup band, Iterable<String> keys) {
-    Map<String, ReportElement> reportElements = new HashMap<>();
+  public static List<JRElement> getBandElementsByKeys(JRElementGroup band, Iterable<String> keys) {
+    List<JRElement> reportElements = new ArrayList<>();
     keys.forEach(
         key -> {
           JRElement bandElement = band.getElementByKey(key);
@@ -36,56 +33,36 @@ public class CustomiserHelper {
             throw new IllegalArgumentException(
                 String.format("The requested key (%s) does not exists within the report.", key));
           }
-          ReportElement element =
-              new ReportElement(
-                  bandElement.getKey(),
-                  bandElement.getX(),
-                  bandElement.getY(),
-                  bandElement.getWidth(),
-                  bandElement.getHeight());
-          reportElements.put(bandElement.getKey(), element);
+          reportElements.add(bandElement);
         });
     return reportElements;
   }
 
+  public static void updateElement(List<JRElement> columnElements, double rebalanceFactor) {
+    AtomicInteger totalWidth = new AtomicInteger(0);
+    IntStream.range(0, columnElements.size())
+        .forEach(
+            index -> {
+              JRElement column = columnElements.get(index);
+              double newWidth = (column.getWidth() * rebalanceFactor);
+              if (index == 0) {
+                column.setWidth((int) Math.round(newWidth));
+              } else {
+                column.setX(totalWidth.get());
+                column.setWidth((int) Math.round(newWidth));
+              }
+              totalWidth.set(totalWidth.get() + (int) Math.round(newWidth));
+            });
+  }
 
-    // iterates over the keys to build report elements
-    public static List<JRElement> getBandElementsByKeys(
-            JRElementGroup band, Iterable<String> keys) {
-        List<JRElement> reportElements = new ArrayList<>();
-        keys.forEach(
-                key -> {
-                    JRElement bandElement = band.getElementByKey(key);
-                    if (bandElement == null) {
-                        throw new IllegalArgumentException(
-                                String.format("The requested key (%s) does not exists within the report.", key));
-                    }
-                    reportElements.add(bandElement);
-                });
-        return reportElements;
+  public static void hideElements(Iterable<String> keys, JRBand band) {
+    keys.forEach(entry -> CustomiserHelper.hideElementByKey(band, entry));
+  }
+
+    public static List<JRElement> remove(
+            List<String> keys, List<JRElement> bandElements) {
+        List<JRElement> elements = new ArrayList<>(bandElements);
+        elements.removeIf(element -> keys.contains(element.getKey()));
+        return elements;
     }
-
-    public static void updateElement(List<JRElement> columnElements, double rebalanceFactor) {
-        Integer[] totalWidth = { 0 };
-        IntStream.range(0, columnElements.size())
-                .forEach(
-                        index -> {
-                            JRElement column = columnElements.get(index);
-                            double newWidth = (column.getWidth() * rebalanceFactor);
-                            if (index == 0) {
-                                column.setWidth((int) Math.round(newWidth));
-                            } else {
-                                column.setX(totalWidth[0]);
-                                column.setWidth((int) Math.round(newWidth));
-                            }
-                            totalWidth[0] = totalWidth[0] + (int) Math.round(newWidth);
-                        });
-    }
-
-    public static void hideElements(List<String> keys, JRBand band) {
-      keys.forEach(entry -> CustomiserHelper.hideElementByKey(band, entry));
-    }
-
-
-
 }
--- src/test/java/iad/transact/reports/testreport/TestReportCustomiser.java
+++ src/test/java/iad/transact/reports/testreport/TestReportCustomiser.java
@@ -3,72 +3,44 @@ package iad.transact.reports.testreport;
 import iad.reports.ColumnFieldPair;
 import iad.reports.Customiser;
 import iad.reports.CustomiserHelper;
+import java.util.*;
 import net.sf.jasperreports.engine.JRBand;
 import net.sf.jasperreports.engine.JRElement;
 import net.sf.jasperreports.engine.design.JasperDesign;
 
-import java.util.*;
-import java.util.stream.IntStream;
-
 public class TestReportCustomiser implements Customiser<TestReportFakeData> {
 
-    private static final List<ColumnFieldPair> COLUMN_FIELD_PAIRS =
-            List.of(new ColumnFieldPair("nameColumnKey","nameFieldKey")
-                    ,new ColumnFieldPair("emailColumnKey","emailFieldKey")
-                    ,new ColumnFieldPair("osBalColumnKey","osBalFieldKey")
-                    ,new ColumnFieldPair("currentDateColumnKey","currentDateFieldKey")
-                    ,new ColumnFieldPair("phoneNumberColumnKey","phoneNumberFieldKey")
-    );
+  private static final List<ColumnFieldPair> COLUMN_FIELD_PAIRS =
+      List.of(
+          new ColumnFieldPair("nameColumnKey", "nameFieldKey"),
+          new ColumnFieldPair("emailColumnKey", "emailFieldKey"),
+          new ColumnFieldPair("osBalColumnKey", "osBalFieldKey"),
+          new ColumnFieldPair("currentDateColumnKey", "currentDateFieldKey"),
+          new ColumnFieldPair("phoneNumberColumnKey", "phoneNumberFieldKey"));
 
-    private static final List<String> MIFID_COLUMN_KEYS = List.of("osBalColumnKey", "currentDateColumnKey");
-    private static final List<String> MIFID_FIELD_KEYS = List.of("osBalFieldKey", "currentDateFieldKey");
+  @Override
+  public void customise(TestReportFakeData data, JasperDesign design) {
+    JRBand columnHeaderBand = design.getColumnHeader();
+    JRBand detailBand = design.getDetailSection().getBands()[0];
+    List<String> columnKeys = new ArrayList<>();
+    COLUMN_FIELD_PAIRS.forEach(key -> columnKeys.add(key.getColumnKey()));
+    List<String> fieldKeys = new ArrayList<>();
+    COLUMN_FIELD_PAIRS.forEach(key -> fieldKeys.add(key.getFieldKey()));
+      rebalance(
+        columnHeaderBand, List.of("osBalColumnKey", "currentDateColumnKey"), columnKeys);
+      rebalance(detailBand, List.of("osBalFieldKey", "currentDateFieldKey"), fieldKeys);
+  }
 
-
-    @Override
-    public void customise(TestReportFakeData data, JasperDesign design) {
-        JRBand columnHeaderBand = design.getColumnHeader();
-        JRBand detailBand = design.getDetailSection().getBands()[0];
-        List<String> columnKeys = new ArrayList<>();
-        COLUMN_FIELD_PAIRS.forEach(key -> columnKeys.add(key.getColumnKey()));
-        List<String> fieldKeys = new ArrayList<>();
-        COLUMN_FIELD_PAIRS.forEach(key -> fieldKeys.add(key.getFieldKey()));
-
-        //List<JRElement> bandElements = CustomiserHelper.getBandElementsByKeys(columnHeaderBand, columnKeys);
-        //int tableHeaderWidth = bandElements.stream().mapToInt(JRElement::getWidth).sum();
-        //List<JRElement> nonMifidTableElements = getNonMifidTableElements(MIFID_COLUMN_KEYS,bandElements);
-        //int nonMifidTableHeaderWidth = nonMifidTableElements.stream().mapToInt(JRElement::getWidth).sum();
-        //double rebalanceFactor = (double) tableHeaderWidth / nonMifidTableHeaderWidth;
-        //CustomiserHelper.hideElements(MIFID_COLUMN_KEYS, columnHeaderBand);
-        //CustomiserHelper.updateElement(nonMifidTableElements, rebalanceFactor);
-
-
-        rebalanceElements(columnHeaderBand,MIFID_COLUMN_KEYS, columnKeys);
-        rebalanceElements(detailBand,MIFID_FIELD_KEYS,fieldKeys);
-    }
-
-    private void rebalanceElements(JRBand band, List<String> excludeKeys, List<String> elementKeys){
-        List<JRElement> bandElements = CustomiserHelper.getBandElementsByKeys(band, elementKeys);
-        int tableHeaderWidth = bandElements.stream().mapToInt(JRElement::getWidth).sum();
-        List<JRElement> nonMifidTableElements = getNonMifidTableElements(excludeKeys,bandElements);
-        int nonMifidTableHeaderWidth = nonMifidTableElements.stream().mapToInt(JRElement::getWidth).sum();
-        double rebalanceFactor = (double) tableHeaderWidth / nonMifidTableHeaderWidth;
-        CustomiserHelper.hideElements(excludeKeys, band);
-        CustomiserHelper.updateElement(nonMifidTableElements, rebalanceFactor);
-    }
-
-    private List<JRElement> getNonMifidTableElements(List<String> excludeKeys,List<JRElement> bandElements){
-        List<JRElement> nonMifidTableElements = new ArrayList<>(bandElements);
-        Iterator<JRElement> itr = nonMifidTableElements.iterator();
-        while(itr.hasNext()) {
-            JRElement nextElement = itr.next();
-            if (excludeKeys.contains(nextElement.getKey())) {
-                itr.remove();
-            }
-        }
-        return nonMifidTableElements;
-    }
-
-
+  private void rebalance(JRBand band, List<String> excludeKeys, List<String> elementKeys) {
+    List<JRElement> bandElements = CustomiserHelper.getBandElementsByKeys(band, elementKeys);
+    int tableHeaderWidth = bandElements.stream().mapToInt(JRElement::getWidth).sum();
+    List<JRElement> nonMifidTableElements = CustomiserHelper.remove(excludeKeys, bandElements);
+    int nonMifidTableHeaderWidth =
+        nonMifidTableElements.stream().mapToInt(JRElement::getWidth).sum();
+    double rebalanceFactor = (double) tableHeaderWidth / nonMifidTableHeaderWidth;
+    CustomiserHelper.hideElements(excludeKeys, band);
+    CustomiserHelper.updateElement(nonMifidTableElements, rebalanceFactor);
+  }
 
 
 }
